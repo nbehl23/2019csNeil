15bb4de4d26d8461a6aed5ddcd2f9bd546c62b19
nbehl23 <nbehl23@nscds.org>
2020-01-31 21:03:58 +0000

    Big Race #3

--- The Big Race/bigrace.js
+++ The Big Race/bigrace.js
@@ -11,14 +11,21 @@ function init(){
 
 
 function startRace(){
-    TweenMax.to("#car2",3,{rotation:360,repeat:0,x:696,ease:Sine.easeOut});
-    TweenMax.to("#car2",2,{x:540, y:-345,delay:1.7, ease:Sine.easeOut});
+    TweenMax.to("#car2",3,{ x:696,ease:Sine.easeOut});
+    TweenMax.to("#car2",.5,{rotation:90, delay:1.5, ease:Sine.easeOut});
+    TweenMax.to("#car2",2,{x:540, y:-345,delay:1.7, ease:Sine.easeOut,});
+    TweenMax.to("#car2",.5,{rotation:270, delay:2.6, ease:Sine.easeOut});
     TweenMax.to("#car2",3,{x:-70, y:-350,delay:2.8, ease:Sine.easeOut});
     TweenMax.to("#car2",3,{x:-75, y:10,delay:5, ease:Sine.easeOut});
-    TweenMax.to("#car2",3,{x:120, y:0,delay:7, ease:Sine.easeOut});
-    TweenMax.to("#car1",2,{x:520,ease:Sine.easeOut});
-    TweenMax.to("#car1",1.5,{x:520, y:-320,delay:1.7, ease:Sine.easeOut});
+    TweenMax.to("#car2",.5,{rotation:180,repeat:0, delay: 7, ease:Sine.easeOut});
+    TweenMax.to("#car2",2,{x:120, y:0,delay:7.1, ease:Sine.easeOut});
+    TweenMax.to("#car1",2,{x:520, y:-10, ease:Sine.easeOut});
+    TweenMax.to("#car1",.5,{rotation:90, delay:1.5, ease:Sine.easeOut});
+    TweenMax.to("#car1",1.5,{x:520, y:-360, delay:2, ease:Sine.easeOut});
+    TweenMax.to("#car1",.5,{rotation:180, delay:3, ease:Sine.easeOut});
     TweenMax.to("#car1",2,{x:-50, y:-300,delay:3, ease:Sine.easeOut});
-    TweenMax.to("#car1",2,{x:-50, y:0,delay:5, ease:Sine.easeOut});
+    TweenMax.to("#car1",.5,{rotation:270, delay:5, ease:Sine.easeOut});
+    TweenMax.to("#car1",2,{x:-50, y:0,delay:5.5, ease:Sine.easeOut});
+    TweenMax.to("#car1",.5,{rotation:180, delay:6.5, ease:Sine.easeOut});
     TweenMax.to("#car1",2,{x:120, y:0,delay:6.5, ease:Sine.easeOut});
 }
