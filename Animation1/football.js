43d37e6c2792f168a423b50698786dfb37853fe6
nbehl23 <nbehl23@nscds.org>
2020-02-11 21:03:50 +0000

    Animationday3

--- Animation1/football.js
+++ Animation1/football.js
@@ -8,6 +8,10 @@ function startRace(){
         var diameter = 400 ; 
     TweenMax.to("#football", 2.5 ,{
         bezier:{curviness:1.5,values:[{x:0,y:0},{x:-diameter/2,y:-diameter/2},{x:-diameter,y:0}]}
-        ,rotation:1080,delay:.1,ease:Power0.easeNone});
-    TweenMax.to("player",3,{x:720,delay:2.5,ease:Power0.easeNone});
+        ,rotation:1080,delay:.1,ease:Power0.easeNone,onComplete:disappear});
+    TweenMax.to("#player",3,{x:550,delay:2.5,ease:Power0.easeNone});
+    
+    }
+function disappear(){
+    document.getElementById("football").style.display = "none";
 }
