d9c87bc6ba2ff594c9a77b75770f0e972d1b771a
nbehl23 <nbehl23@nscds.org>
2020-02-26 19:22:31 +0000

    day3

--- First Game/fieldgoalgame.js
+++ First Game/fieldgoalgame.js
@@ -16,7 +16,7 @@ $(document).keydown(
             case 37: 
                 
                     football.finish().animate({
-                        left: "-=10"
+                        left: "-=10",
                     });
                 break;
             case 38: 
@@ -26,7 +26,7 @@ $(document).keydown(
                 break;
             case 39: 
                     football.finish().animate({
-                        right: "-=10"
+                        left: "-=-10"
                     });
                 break;
             case 40:
@@ -36,16 +36,28 @@ $(document).keydown(
                 break;						
         }
     }
-			/*function moveLeft(){
-				objImage.style.left=parseInt(objImage.style.left)-5 +'px';
-			}
-			function moveUp(){
-				objImage.style.top=parseInt(objImage.style.top)-5 +'px';
-			}
-			function moveRight(){
-				objImage.style.left=parseInt(objImage.style.left)+5 +'px';
-			}
-			function moveDown(){
-				objImage.style.top=parseInt(objImage.style.top)+5 +'px';
-			}	*/	
 )
+function getPositions(football) {
+  var $box = $("#football");
+  var pos = $box.position();
+  var width = $box.width();
+  var height = $box.height();
+  return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
+}
+        
+function comparePositions(p1, p2) {
+  var x1 = p1[0] < p2[0] ? p1 : p2;
+  var x2 = p1[0] < p2[0] ? p2 : p1;
+  return x1[1] > x2[0] || x1[0] === x2[0] ? true : false;
+}
+
+function checkCollisions(){
+  var box = $("#toscore")[0];
+  var pos = getPositions(box);
+
+  var pos2 = getPositions(this);
+  var horizontalMatch = comparePositions(pos[0], pos2[0]);
+  var verticalMatch = comparePositions(pos[1], pos2[1]);            
+  var match = horizontalMatch && verticalMatch;
+  if (match) { $("body").append("<p>COLLISION !!!</p>"); }
+}
