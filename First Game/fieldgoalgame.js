2f27feccc3eb969c0fe8656fac4ea635fd30d1ce
nbehl23 <nbehl23@nscds.org>
2020-03-03 15:31:17 +0000

    day3ofcollision

--- First Game/fieldgoalgame.js
+++ First Game/fieldgoalgame.js
@@ -44,14 +44,15 @@ function getPositions(element) {
     var width = elementObj.width();
     var height = elementObj.height();
     var results = [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
-    console.log(results);
+    //console.log(results);
     return results;
 }
         
-function comparePositions(p1, p2) {
-  var x1 = p1[0] < p2[0] ? p1 : p2;
-  var x2 = p1[0] < p2[0] ? p2 : p1;
-  return x1[1] > x2[0] || x1[0] === x2[0] ? true : false;
+function comparePositionsx() {
+    if (pos[0]>pos2[0]) true
+}
+function comparePositionsy(){
+    if (pos[1]>pos2[1]) true
 }
 
 function checkCollisions(){
@@ -59,11 +60,11 @@ function checkCollisions(){
   var box = $("#toscore")[0];
   var pos = getPositions(box);
 
-  var pos2 = getPositions(football);
-  var horizontalMatch = comparePositions(pos[0], pos2[0]);
-  var verticalMatch = comparePositions(pos[1], pos2[1]);            
+  var pos2 = getPositions(this);
+  var horizontalMatch = comparePositionsx(pos[0], pos2[0]);
+  var verticalMatch = comparePositionsy(pos[1], pos2[1]);            
   var match = horizontalMatch && verticalMatch;
-  if (match) {disappear;}
+  if (match) {disappear();}
 }
 
 function disappear(){
